62e12d2bd1801e02e1c728d0558fff1744cc22f4
VarshithNaiduLPU <varshithnaidu@lpu.in>
2024-09-24 11:36:12 +0530

    Small Text Changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-        <h2>Goto <Link href="/question1">Question 1</Link></h2>
-        <h2>Goto <Link href="/question2">Question 2</Link></h2>
+        <h2>Go to <Link href="/question1">Question 1</Link></h2>
+        <h2>Go to <Link href="/question2">Question 2</Link></h2>
     </div>
 );
 }
